e8f9e38b6d0513a9696bdce8b2d208ce14ec9774
Daniel Newton <kdaniel.newton@gmail.com>
2025-06-01 23:09:11 -0500

    Add Astro blog application with initial setup
    - Created foundational folder structure and configurations for the Astro blog app.
    - Added layouts, components, and pages including basic blog and about pages.
    - Configured Astro content collection schema, site metadata, and integrations.
    - Included global stylesheet and necessary assets.

---
+++ apps/blog/src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your app by using the `import` keyword.
+
+export const SITE_TITLE = 'Astro Blog';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
