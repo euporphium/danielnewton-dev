496bf6054a515c4d40d1eef85141d1e1d009de30
Daniel Newton <kdaniel.newton@gmail.com>
2025-06-01 23:26:05 -0500

    Add tailwindcss

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://danielnewton.dev',
   // base: '/blog',
   integrations: [mdx(), sitemap()],
+  vite: { plugins: [tailwindcss()] },
 });
